67f39532558b4dc23c6e235e78b7c0061545359d
Trideb <panigrahitrideb1@gmail.com>
2026-05-26 01:40:23 +0530

    Added User Schema

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  Column,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  role!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
